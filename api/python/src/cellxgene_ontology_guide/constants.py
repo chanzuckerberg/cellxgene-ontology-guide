9baf5a5eac3f520d5356327333d4befd9fcc03fd
nayib-jose-gloria <ngloria@chanzuckerberg.com>
2024-02-27 12:10:00 -0500

    pr feedback

--- api/python/src/cellxgene_ontology_guide/constants.py
+++ api/python/src/cellxgene_ontology_guide/constants.py
@@ -3,7 +3,7 @@ import os
 PACKAGE_ROOT = os.path.dirname(os.path.realpath(__file__))
 ALL_ONTOLOGY_FILENAME = "all_ontology.json.gz"
 ONTOLOGY_INFO_FILENAME = "ontology_info.yml"
-ONTOLOGY_ASSET_RELEASE_URL = "https://api.github.com/repos/chanzuckerberg/cellxgene-ontology-guide/releases"
+ONTOLOGY_ASSET_RELEASE_URL = "https://github.com/chanzuckerberg/cellxgene-ontology-guide/releases/download"
 SCHEMA_VERSION_TO_ONTOLOGY_ASSET_TAG = {
-    "5.0.0": "v0.0.1"
+    "5.0.0": "ontology-assets-v0.0.1"
 }
