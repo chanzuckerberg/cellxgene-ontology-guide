c52c436de8b7c919bcf063dc2c4be79a146ad0f4
Trent Smith <1429913+Bento007@users.noreply.github.com>
2024-03-25 10:00:00 -0700

    chore: enable code coverage (#143)
    ## Reason for Change
    enable code coverage to help maintain a high coding standard
    
    # Changes
    - enable code coverage to the python api and ontology-builder
    - combine the coverage reports and submit to codecoverage.
    - setup a GHA run to run codecoverge for prs and main

---
+++ tools/coverage.mk
@@ -0,0 +1,17 @@
+# shared coverage targets
+REPO_ROOT := $(shell git rev-parse --show-toplevel)
+COVERAGE_DATA_FILE=.coverage.$(shell git rev-parse --short HEAD)
+export COVERAGE_RUN_ARGS:=--data-file=$(REPO_ROOT)/$(COVERAGE_DATA_FILE) --branch --parallel-mode
+
+
+coverage/run:
+	coverage run $(COVERAGE_RUN_ARGS) -m pytest ./tests;
+
+coverage/combine:
+	coverage combine --data-file=$(COVERAGE_DATA_FILE)
+
+coverage/report-xml: coverage/combine
+	coverage xml --data-file=$(COVERAGE_DATA_FILE) -i --skip-empty
+
+coverage/report-html: coverage/combine
+	coverage html --data-file=$(COVERAGE_DATA_FILE) -i --skip-empty
