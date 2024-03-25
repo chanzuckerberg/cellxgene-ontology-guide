29f722aff1a7e57bdb3df63d06025ea2d973f468
Bento007 <trent.smith007@gmail.com>
2024-03-25 11:06:45 -0700

    fix documentation

--- api/python/src/cellxgene_ontology_guide/__init__.py
+++ api/python/src/cellxgene_ontology_guide/__init__.py
@@ -6,3 +6,4 @@
 import cellxgene_ontology_guide._version as version
 
 __version__ = version.__version__
+__all__ = ["curated_ontology_term_lists", "entities", "ontology_parser", "supported_versions"]
