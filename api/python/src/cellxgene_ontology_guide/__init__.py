75c1a04a362a61ede47ce9f1a0670199b61455e4
Bento007 <trent.smith007@gmail.com>
2024-04-03 15:28:51 -0700

    update tags

--- api/python/src/cellxgene_ontology_guide/__init__.py
+++ api/python/src/cellxgene_ontology_guide/__init__.py
@@ -1,9 +1,9 @@
 """
-![PyPI](https://img.shields.io/pypi/v/cellxgene-ontology-guide?label=pypi%20package)
-![PyPI - Downloads](https://img.shields.io/pypi/dm/cellxgene-ontology-guide)
+[![PyPI](https://img.shields.io/pypi/v/cellxgene-ontology-guide?label=pypi%20package)](https://pypi.org/project/cellxgene-ontology-guide/)
+[![PyPI - Downloads](https://img.shields.io/pypi/dm/cellxgene-ontology-guide)](<(https://pypi.org/project/cellxgene-ontology-guide/)>)
 [![codecov](https://codecov.io/gh/chanzuckerberg/cellxgene-ontology-guide/graph/badge.svg?token=ceXxNPx22I)](https://codecov.io/gh/chanzuckerberg/cellxgene-ontology-guide)
 ![example workflow](https://github.com/chanzuckerberg/cellxgene-ontology-guide/actions/workflows/push-tests.yml/badge.svg?branch=main)
-![GitHub](https://img.shields.io/github/license/chanzuckerberg/cellxgene-ontology-guide)
+[![GitHub](https://img.shields.io/github/license/chanzuckerberg/cellxgene-ontology-guide)](./LICENSE)
 
 # cellxgene-ontology-guide
 A Python package to help with ontology term mapping in for projects in the [cellxgene](https://cellxgene.cziscience.com/) ecosystem.
