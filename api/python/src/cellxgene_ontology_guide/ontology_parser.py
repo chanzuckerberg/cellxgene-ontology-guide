3c3f5ab13fa6fd5e8bf0c92b6887bc298f00ecd9
Bento007 <trent.smith007@gmail.com>
2024-05-13 10:56:11 -0700

    fix tests

--- api/python/src/cellxgene_ontology_guide/ontology_parser.py
+++ api/python/src/cellxgene_ontology_guide/ontology_parser.py
@@ -591,8 +591,8 @@ class OntologyParser:
         Example
         >>> from cellxgene_ontology_guide.ontology_parser import OntologyParser
         >>> ontology_parser = OntologyParser()
-        >>> ontology_parser.map_term_synonyms(["CL:0000001", "CL:0000019"])
-        {'CL:0000001', ['cell Beta', 'cell Bravo'], 'CL:0000019': ['sperm cell', 'spermatozoid', 'spermatozoon']}
+        >>> ontology_parser.map_term_synonyms(["CL:0000005", "CL:0000019"])
+        {'CL:0000005': ['fibroblast neural crest derived'], 'CL:0000019': ['sperm cell', 'spermatozoid', 'spermatozoon']}
 
         :param term_ids: list of str ontology terms to fetch synonyms for
         :return: Dict[str, List[str]] mapping term IDs to their respective synonym lists
